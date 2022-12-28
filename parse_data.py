a460cf44e54602c5c52475c59c90ef6dcf422fd4
Vineet Keshari <vineet.keshari@gmail.com>
2022-12-27 21:19:31 -0800

    Fix parsing, change data to counts. Add map data.

--- parse_data.py
+++ parse_data.py
@@ -3,30 +3,32 @@ import pickle
 import xml.etree.ElementTree as ET
 from datetime import date
 
-PRECISION_FMT = "%.4f"
-DATA_DIR = '../apple_health_export/workout-routes'
+PRECISION = 4
+PRECISION_FMT = "%." + str(PRECISION) + "f"
+OFFSET = pow(10.0, -PRECISION) / 2
+ROUTE_DATA_DIR = '../apple_health_export/workout-routes'
 
-for f in sorted(os.listdir(DATA_DIR)):
-  tree = ET.parse(DATA_DIR + '/' + f)
+pts_to_dates = {}
+
+for f in sorted(os.listdir(ROUTE_DATA_DIR)):
+  tree = ET.parse(ROUTE_DATA_DIR + '/' + f)
   root = tree.getroot()
 
   time_string = root[0][0].text
   d = date.fromisoformat(time_string[:10])
 
-  pts_to_dates = {}
-
   for p in root[1][1]:
-    (lon, lat) = (PRECISION_FMT % eval(p.attrib['lon']), PRECISION_FMT % eval(p.attrib['lat']))
+    (lon, lat) = (eval(PRECISION_FMT % eval(p.attrib['lon'])) + OFFSET, eval(PRECISION_FMT % eval(p.attrib['lat'])) + OFFSET)
     ele = eval(p[0].text)
 
     if (lon, lat) not in pts_to_dates:
-      pts_to_dates[(lon, lat)] = {d}
+      pts_to_dates[(lon, lat)] = 1
     else:
-      pts_to_dates[(lon, lat)].add(d)
+      pts_to_dates[(lon, lat)] += 1
 
-  print(f + '\t' + str(len(pts_to_dates)) + " points")
+  print(f + '\t' + 'Total points: ' + str(len(pts_to_dates)))
 
 OUT_FILENAME = 'pts_to_dates.data'
 with open('pts_to_dates.data', 'wb+') as f:
   pickle.dump(pts_to_dates, f)
-  print("Data written to {}".format(OUT_FILENAME))
+  print("Route data written to {}".format(OUT_FILENAME))
--- show_route_map.py
+++ show_route_map.py
@@ -6,6 +6,9 @@ import cartopy.io.img_tiles as cimgt
 import matplotlib.pyplot as plt
 
 IN_FILENAME = 'pts_to_dates.data'
+OUT_MAP_FILENAME = 'sf_map.png'
+IMAGE_DPI = 80
+IMAGE_SIZE = 13.5
 
 with open(IN_FILENAME, 'rb') as f:
   pts_to_dates = pickle.load(f)
@@ -13,9 +16,25 @@ with open(IN_FILENAME, 'rb') as f:
 
 IMAGERY = cimgt.OSM(desired_tile_form = 'L')
 
-fig = plt.figure()
-ax = fig.add_subplot(1, 1, 1, projection=IMAGERY.crs)
-ax.set_extent([-122.52, -122.37, 37.72, 37.85], crs=ccrs.PlateCarree())
-ax.add_image(IMAGERY, 14, cmap = 'Greys_r')
+fig = plt.figure(figsize=(IMAGE_SIZE, IMAGE_SIZE), dpi = IMAGE_DPI)
+ax = fig.add_subplot(1, 1, 1, projection = IMAGERY.crs)
+ax.set_extent([-122.52, -122.37, 37.72, 37.85], crs = ccrs.PlateCarree())
+ax.add_image(IMAGERY, 14, cmap = 'Greys_r', alpha = 1.0)
+
+xs = []
+ys = []
+vals = []
+
+for (lon, lat), dates in pts_to_dates.items():
+  xs.append(lon)
+  ys.append(lat)
+  vals.append(dates)
+
+ax.scatter(x = xs, y = ys, s = 5.0,
+            marker = 's', c = vals, cmap = 'terrain_r',
+            vmin = 0, vmax = 100, alpha = 0.5,
+            transform = ccrs.PlateCarree())
 
 plt.show()
+fig.savefig(OUT_MAP_FILENAME, dpi = IMAGE_DPI)
+print ("Map written to " + OUT_MAP_FILENAME)
