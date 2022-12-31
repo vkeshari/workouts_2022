8a7627b4881fbfec20d7e0d3547beb3b3f326666
Vineet Keshari <vineet.keshari@gmail.com>
2022-12-31 14:43:01 -0800

    Add route map animation

--- parse_route_data.py
+++ parse_route_data.py
@@ -8,6 +8,11 @@ PRECISION_FMT = "%." + str(PRECISION) + "f"
 OFFSET = pow(10.0, -PRECISION) / 2
 ROUTE_DATA_DIR = '../apple_health_export/workout-routes'
 
+LON_MIN = -122.52
+LON_MAX = -122.37
+LAT_MIN = 37.72
+LAT_MAX = 37.83
+
 MIN_DATE = date(2021, 1, 1)
 MAX_DATE = date(2023, 1, 1)
 
@@ -25,6 +30,9 @@ for f in sorted(os.listdir(ROUTE_DATA_DIR)):
   for p in root[1][1]:
     (lon, lat) = (eval(PRECISION_FMT % eval(p.attrib['lon'])) + OFFSET, eval(PRECISION_FMT % eval(p.attrib['lat'])) + OFFSET)
     ele = eval(p[0].text)
+    
+    if lon < LON_MIN or lon > LON_MAX or lat < LAT_MIN or lat > LAT_MAX:
+      continue
 
     if (lon, lat) not in pts_to_dates:
       pts_to_dates[(lon, lat)] = {d}
@@ -34,6 +42,6 @@ for f in sorted(os.listdir(ROUTE_DATA_DIR)):
   print(f + '\t' + 'Total points: ' + str(len(pts_to_dates)))
 
 OUT_FILENAME = 'pts_to_dates.data'
-with open('pts_to_dates.data', 'wb+') as f:
+with open(OUT_FILENAME, 'wb+') as f:
   pickle.dump(pts_to_dates, f)
   print("Route data written to {}".format(OUT_FILENAME))
--- show_route_map.py
+++ show_route_map.py
@@ -6,11 +6,17 @@ import cartopy.io.img_tiles as cimgt
 import matplotlib.pyplot as plt
 import matplotlib.colors as colors
 import matplotlib.image as mpimg
+import matplotlib.animation as anim
+
+from datetime import date, timedelta
 
 IN_FILENAME = 'pts_to_dates.data'
+
 BASEMAP_FILENAME = 'sf_basemap.png'
 CUT_BASEMAP_FILENAME = 'sf_basemap_cut.png'
+
 OUT_MAP_FILENAME = 'sf_map.png'
+
 IMAGE_DPI = 80
 IMAGE_SIZE = 13.5 * 2
 
@@ -23,6 +29,13 @@ LAT_MAX = 37.83
 LIMITS_TUPLE = [LON_MIN, LON_MAX, LAT_MIN, LAT_MAX]
 LAT_LON_RATIO = 1.2625
 
+DATA_BY_DAY_FILENAME = 'dates_to_ptval.data'
+OUT_ANIMATION_FILENAME = 'sf_map_animated.mp4'
+ANIMATION_MIN_DATE = date(2021, 1, 1)
+ANIMATION_MAX_DATE = date(2023, 1, 1)
+ANIMATION_FPS = 30
+ONE_DAY = timedelta(days=1)
+
 def build_base_map():
   fig = plt.figure(figsize=(IMAGE_SIZE, IMAGE_SIZE), dpi = IMAGE_DPI)
   ax = fig.add_subplot(1, 1, 1, projection = IMAGERY.crs, frameon=False)
@@ -33,7 +46,7 @@ def build_base_map():
   fig.savefig(BASEMAP_FILENAME, dpi = IMAGE_DPI)
   print ("Basemap written to " + BASEMAP_FILENAME)
 
-def build_dynamic_map():
+def build_static_map():
   basemap = mpimg.imread(CUT_BASEMAP_FILENAME)
   print ("Basemap read from " + CUT_BASEMAP_FILENAME)
   fig = plt.figure(figsize=(IMAGE_SIZE, IMAGE_SIZE), dpi = IMAGE_DPI)
@@ -54,9 +67,7 @@ def build_dynamic_map():
     xs.append(lon)
     ys.append(lat)
     vals.append(len(dates))
-    print (dates)
 
-  fig.set_size_inches(IMAGE_SIZE, IMAGE_SIZE)
   hexplot = ax.hexbin(x = xs, y = ys, C = vals, gridsize = 300,
                       cmap = 'gist_earth_r', alpha = 0.9, linewidths = 0,
                       norm = colors.LogNorm(vmin = 0.5, vmax = 500))
@@ -76,5 +87,88 @@ def build_dynamic_map():
   fig.savefig(OUT_MAP_FILENAME, dpi = IMAGE_DPI)
   print ("Map written to " + OUT_MAP_FILENAME)
 
+def build_dynamic_map(rebuild_animation_data = True):
+  basemap = mpimg.imread(CUT_BASEMAP_FILENAME)
+  print ("Basemap read from " + CUT_BASEMAP_FILENAME)
+  fig = plt.figure(figsize=(IMAGE_SIZE, IMAGE_SIZE), dpi = IMAGE_DPI)
+
+  if rebuild_animation_data:
+    with open(IN_FILENAME, 'rb') as f:
+      pts_to_dates = pickle.load(f)
+      print("Route data read from {}".format(IN_FILENAME))
+
+    print ("Total points: " + str(len(pts_to_dates)))
+
+    date_to_ptvals = {}
+    n = 0
+
+    for (lon, lat), dates in pts_to_dates.items():
+      n += 1
+      if lon < LON_MIN or lon > LON_MAX or lat < LAT_MIN or lat > LAT_MAX:
+        continue
+      for date in dates:
+        d = date
+        while d < ANIMATION_MAX_DATE:
+          if d not in date_to_ptvals:
+            date_to_ptvals[d] = {}
+          if (lon, lat) not in date_to_ptvals[d]:
+            date_to_ptvals[d][(lon, lat)] = 0
+          date_to_ptvals[d][(lon, lat)] += 1
+          d += ONE_DAY
+
+      #print ("Processed " + "(" + str(lon) + ", " + str(lat) + ")" + " with no. of days: " + str(len(dates)))
+      if n % 1000 == 0:
+        print ("Processed " + str(n) + " points")
+
+    with open(DATA_BY_DAY_FILENAME, 'wb+') as f:
+      pickle.dump(date_to_ptvals, f)
+      print("Animated route data written to {}".format(DATA_BY_DAY_FILENAME))
+
+  with open(DATA_BY_DAY_FILENAME, 'rb') as f:
+    date_to_ptvals = pickle.load(f)
+    print("Animated route data read from {}".format(DATA_BY_DAY_FILENAME))
+
+  moviewriter = anim.FFMpegWriter(fps = ANIMATION_FPS)
+  with moviewriter.saving(fig, OUT_ANIMATION_FILENAME, dpi = IMAGE_DPI):
+    d = ANIMATION_MIN_DATE
+    while d < ANIMATION_MAX_DATE:
+      print (str(d) + " has No. of points: " + str(len(date_to_ptvals[d])))
+
+      xs = []
+      ys = []
+      vals = []
+      for (lon, lat), val in date_to_ptvals[d].items():
+        xs.append(lon)
+        ys.append(lat)
+        vals.append(val)
+
+      ax = fig.add_subplot(1, 1, 1)
+      fig.tight_layout()
+
+      ax.imshow(basemap, extent = LIMITS_TUPLE, aspect = LAT_LON_RATIO, alpha = 1.0)
+
+      hexplot = ax.hexbin(x = xs, y = ys, C = vals, gridsize = 300,
+                          cmap = 'gist_earth_r', alpha = 0.9, linewidths = 0,
+                          norm = colors.LogNorm(vmin = 0.5, vmax = 500))
+
+      cbar = plt.colorbar(hexplot,
+                          shrink = 0.75, aspect = 50,
+                          format = "%d", pad = 0.01,
+                          ticks = [1, 2, 5, 10, 20, 50, 100, 200, 500])
+      cbar.ax.tick_params(labelsize = 50)
+      cbar.set_label("No. of Days", size = 50)
+
+      ax.text(-122.40, 37.825, d.isoformat(), fontsize = 50)
+
+      plt.title("Walking in San Francisco 2021-22", fontsize = 80)
+      plt.tick_params(left = False, right = False, labelleft = False,
+                      labelbottom = False, bottom = False)
+
+      moviewriter.grab_frame()
+      d += ONE_DAY
+
+  print ("Animation written to " + OUT_ANIMATION_FILENAME)
+
 #build_base_map() # needs cropping
-build_dynamic_map()
+#build_static_map()
+build_dynamic_map(rebuild_animation_data = True)
