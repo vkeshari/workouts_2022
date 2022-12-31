d92fca60d4c47f33ffc2236966f63803ec2dc9c4
Vineet Keshari <vineet.keshari@gmail.com>
2022-12-30 18:10:31 -0800

    Change to hex plot, add colorbar and scaling

--- show_route_map.py
+++ show_route_map.py
@@ -4,11 +4,18 @@ import cartopy.crs as ccrs
 import cartopy.io.img_tiles as cimgt
 
 import matplotlib.pyplot as plt
+import matplotlib.colors as colors
 
 IN_FILENAME = 'pts_to_dates.data'
 OUT_MAP_FILENAME = 'sf_map.png'
 IMAGE_DPI = 80
 IMAGE_SIZE = 13.5
+
+LON_MIN = -122.52
+LON_MAX = -122.37
+LAT_MIN = 37.72
+LAT_MAX = 37.83
+LIMITS_TUPLE = [LON_MIN, LON_MAX, LAT_MIN, LAT_MAX]
 
 with open(IN_FILENAME, 'rb') as f:
   pts_to_dates = pickle.load(f)
@@ -17,8 +24,8 @@ with open(IN_FILENAME, 'rb') as f:
 IMAGERY = cimgt.OSM(desired_tile_form = 'L')
 
 fig = plt.figure(figsize=(IMAGE_SIZE, IMAGE_SIZE), dpi = IMAGE_DPI)
-ax = fig.add_subplot(1, 1, 1, projection = IMAGERY.crs)
-ax.set_extent([-122.52, -122.37, 37.72, 37.85], crs = ccrs.PlateCarree())
+ax = fig.add_subplot(1, 1, 1, projection = IMAGERY.crs, frameon=False)
+ax.set_extent(LIMITS_TUPLE, crs = ccrs.PlateCarree())
 ax.add_image(IMAGERY, 14, cmap = 'Greys_r', alpha = 1.0)
 
 xs = []
@@ -26,14 +33,24 @@ ys = []
 vals = []
 
 for (lon, lat), dates in pts_to_dates.items():
+  if lon < LON_MIN or lon > LON_MAX or lat < LAT_MIN or lat > LAT_MAX:
+    continue
   xs.append(lon)
   ys.append(lat)
   vals.append(dates)
 
-ax.scatter(x = xs, y = ys, s = 5.0,
-            marker = 's', c = vals, cmap = 'terrain_r',
-            vmin = 0, vmax = 100, alpha = 0.5,
-            transform = ccrs.PlateCarree())
+hexplot = ax.hexbin(x = xs, y = ys, C = vals, gridsize = 300,
+                    cmap = 'gist_earth_r', alpha = 0.8, linewidths = 0,
+                    norm = colors.LogNorm(vmin = 0.5, vmax = 500),
+                    transform = ccrs.PlateCarree())
+cbar = plt.colorbar(hexplot,
+                    shrink = 0.85, aspect = 50,
+                    format = "%d", pad = 0.01,
+                    ticks = [1, 2, 5, 10, 50, 100, 500])
+cbar.ax.tick_params(labelsize = 20)
+cbar.set_label("No. of Days", size = 20)
+
+plt.title("Walking in San Francisco 2022-23", fontsize = 30)
 
 plt.show()
 fig.savefig(OUT_MAP_FILENAME, dpi = IMAGE_DPI)
